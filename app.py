4f7d1317054f5801a121d3762c138c4a0d1fbef1
KashifAli-coder <silkkashif@gmail.com>
2026-01-08 17:43:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import io
 # ================= CONFIGURATION =================
 SETTINGS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRtyPndRTxFA2DFEiAe7GYsXm16HskK7a40oc02xfwGNuRWTtMgHNrA2aSLZb3K6tTA5sM9Lt_nDc3q/pub?gid=1215788411&single=true&output=csv"
 ORDERS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRtyPndRTxFA2DFEiAe7GYsXm16HskK7a40oc02xfwGNuRWTtMgHNrA2aSLZb3K6tTA5sM9Lt_nDc3q/pub?gid=0&single=true&output=csv"
-SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyIVWmY0Cj8_S9W-fdwRFWnE6cg7TxTrKqxtvNjSS330krT-VuYtesLcdpD_n5tStXv/exec"
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwOKzymo0Cl84EM5nCnwg3KnCSLuO6jI6CrvRI6GCXI7-vMVPTMQ2RFgTnbXj18jAsY/exec"
 ADMIN_WHATSAPP = "923005508112"
 
 # --- PDF GENERATOR (FIXED) ---
